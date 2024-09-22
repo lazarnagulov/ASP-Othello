1c68a7ef2f974cf92f7e6d749b693d9b22f579ea
Lazar Nagulov <lazarnagulov1@gmail.com>
2024-09-22 12:03:33 +0200

    refactor: Rename interface to ui

--- src/main.py
+++ src/main.py
@@ -5,17 +5,17 @@ from ui.gui import GUI
 import sys
 
 def main() -> None:
-    interface: Optional[UserInterface] = None
+    ui: Optional[UserInterface] = None
     
     if len(sys.argv) != 1:
         match sys.argv[1]:
-            case "--console" | "-c": interface = ConsoleInterface()
-            case "--ui" | "-u": interface = GUI()
-            case _: interface = GUI()
+            case "--console" | "-c": ui = ConsoleInterface()
+            case "--ui" | "-u": ui = GUI()
+            case _: ui = GUI()
     else:
-        interface = ConsoleInterface()
+        ui = ConsoleInterface()
 
-    interface.run()
+    ui.run()
     
 if __name__ == "__main__":
     main()
